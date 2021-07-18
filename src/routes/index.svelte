727047d49e0ae065581ff58365ce75ce071bb9b5
multicam <jmgiorgi@gmail.com>
2021-07-18 16:34:39 +1000

    - scaffold/1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,14 +12,7 @@
 
 <section>
 	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
+		Hello World
 	</h1>
 
 	<h2>
